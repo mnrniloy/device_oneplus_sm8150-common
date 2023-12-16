c08ba6c1ea96cea6a6c2c88a74dc9412329e8ca1
mnrniloy <mnrniloy0@gmail.com>
2023-12-16 16:30:13 +0600

    sm8150-common: Add Google Camera support

--- common.mk
+++ common.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.fuse.passthrough.enable=true
 
+# Google Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.google.android.GoogleCamera.xml
+  
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
