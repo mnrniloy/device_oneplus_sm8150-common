5401058e9e3a8490c3bf4531462ef8c9fc26a805
John Galt <johngaltfirstrun@gmail.com>
2024-04-12 10:04:10 -0400

    common: enable mimalloc allocator
    We get improved performance and lower rss on mimalloc than jemalloc,
    even with additional security mitigations/protections than jemalloc

--- common.mk
+++ common.mk
@@ -223,6 +223,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Use mimalloc allocator instead of jemalloc
+PRODUCT_USE_MIMALLOC := true
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
