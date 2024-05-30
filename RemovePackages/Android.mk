eafc60d22ee2f482372cccbacde8fba264db5ba2
mnrniloy <mnrniloy0@gmail.com>
2024-05-30 18:44:38 +0600

    sm8150-common: Remove Prebuilt Apps

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+          Aperture \
+          ApertureLensLauncher \
+          Contacts \
+          Eleven \
+          Etar \
+          Glimpse \
+          Jelly \
+          LatinIME \
+          messaging \
+          Seedvault
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
