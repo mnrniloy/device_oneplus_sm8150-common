b4d92f610f9e75b6ad16e341212261cbb985152e
mnrniloy <mnrniloy0@gmail.com>
2024-06-30 23:32:03 +0600

    sm8150-common: OnePlus Apps

--- common.mk
+++ common.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# OnePlus apps
+$(call inherit-product-if-exists, vendor/oneplus/apps/apps-vendor.mk)
+
 # Get Qcom components
 TARGET_EXCLUDE_QCOM_SEPOLICY := true
 TARGET_BOARD_PLATFORM := msmnile
